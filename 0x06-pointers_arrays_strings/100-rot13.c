83d1a80f2c9a5e4294390f9eccca992721ac3392
felixnwanneka8 <chenko.tyler@gmail.com>
2023-10-18 13:02:07 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,56 +1,30 @@
 #include "main.h"
-
 #include <stdio.h>
 
-
 /**
-
- * rot13 - main function
-
+ * rot13 - encoder rot13
+ * @s: pointer to string params
  *
-
- * @s: String poiner
-
- *
-
- * Return: String function
-
+ * Return: *s
  */
 
-
 char *rot13(char *s)
-
 {
+int i;
+int j;
+char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-        int k, r;
-
-        char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-
-        char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-
-        for (k = 0; s[k] != '\0'; k++)
-
-        {
-
-        for (r = 0; r < 52; r++)
-
-        {
-
-        if (s[k] == data1[r])
-
-        {
-
-        s[k] = datarot[r];
-
-        break;
-
-        }
-
-        }
-
-        }
-
-        return (s);
-
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 52; j++)
+{
+if (s[i] == data1[j])
+{
+s[i] = datarot[j];
+break;
+}
+}
+}
+return (s);
 }
